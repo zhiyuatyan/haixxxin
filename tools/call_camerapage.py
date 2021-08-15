76277cabfd766e8d8c86c467d5a3cccd6bfd0a20
zhiyuyan <zhiyu_yan1@163.com>
2021-08-15 15:52:56 +0800

    修改

--- tools/call_camerapage.py
+++ tools/call_camerapage.py
@@ -362,7 +362,7 @@ class Video_Dis(QMainWindow, Ui_MainWindow):
             ret, frame = self.video_display.read()
             frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
             self.video_change_flag = False
-
+            #
             print('old_view : ', self.save_view_2[0], 'new_view : ', self.save_view_2[1])
             new_view, img_local, x_start, y_start = kuashidian.dsd(self.pred_bbox[4], self.pred_bbox[5],
                                                                    self.save_view_2[0],
